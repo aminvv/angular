df360a4dbb51f690fa421fd10fd7f7acf3c3151c
aminvv <aminvier1378@gmail.com>
2022-09-18 14:46:57 +0430

    reactive form Builder  session  8-3

--- src/app/eployee-info/eployee-info.component.ts
+++ src/app/eployee-info/eployee-info.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../module/Employee';
 import { KeyValue } from '../module/grade';
 import { GradeService } from '../services/grade.service';
- import { FormGroup,FormControl,FormBuilder } from '@angular/forms';
+ import { FormGroup,FormControl,FormBuilder ,Validators, MinValidator} from '@angular/forms';
 @Component({
   selector: 'app-eployee-info',
   templateUrl: './eployee-info.component.html',
@@ -12,18 +12,22 @@ export class EployeeInfoComponent implements OnInit {
   grade:KeyValue[] 
   employee:Employee =new Employee(0,"","","","",0,"");
   Coustomer:FormGroup;
+  buliderForm
 
-  constructor(private gradeservice:GradeService) { }
+  constructor(private gradeservice:GradeService ,private fb:FormBuilder ) { }
 
   ngOnInit(): void {
     this.grade=this.gradeservice.GetAll()
 
-    this.Coustomer= new FormGroup({
-      FirstName:new FormControl('aa'),
-      LastName:new FormControl(''),
-      CoustomerCode:new FormControl(''),
-      Email:new FormControl(''),
+    this.Coustomer= this.fb.group({
+      FirstName:['amin',Validators.required],
+      LastName:['' ,[Validators.required,Validators.minLength(2),Validators.maxLength(20)]],
+      CoustomerCode:['',[Validators.required,Validators.minLength(2),Validators.maxLength(20)]],
+      Email:['',[Validators.required,Validators.email]],
+
      })
+
+
 
   }
  save(value){
