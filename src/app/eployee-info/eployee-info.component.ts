2bfd3e87d261f1ef32b7eef8fe2f16c2fbddc420
aminvv <aminvier1378@gmail.com>
2022-10-03 12:31:09 +0330

    Form-Dynamic 8-5

--- src/app/eployee-info/eployee-info.component.ts
+++ src/app/eployee-info/eployee-info.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from '../module/Employee';
 import { KeyValue } from '../module/grade';
 import { GradeService } from '../services/grade.service';
- import { FormGroup,FormControl,FormBuilder ,Validators, MinValidator} from '@angular/forms';
+ import { FormGroup,FormControl,FormBuilder ,Validators, MinValidator, FormArray} from '@angular/forms';
 @Component({
   selector: 'app-eployee-info',
   templateUrl: './eployee-info.component.html',
@@ -12,18 +12,34 @@ export class EployeeInfoComponent implements OnInit {
   grade:KeyValue[] 
   employee:Employee =new Employee(0,"","","","",0,"");
   Coustomer:FormGroup;
-  buliderForm
+  buliderForm :FormGroup;
+  Formtemplate=[{'type':"textBox",'label':"FirstName"},{'type':"textBox",'label':"LastName"},
+  {'type':"select",'label':"City",'option':["teharn","tabirz","urmia"]}]
+
 
   constructor(private gradeservice:GradeService ,private fb:FormBuilder ) { }
 
   ngOnInit(): void {
     this.grade=this.gradeservice.GetAll()
+     let Group={}
+    this.Formtemplate.forEach(q=>{
+      Group[q.label]=new FormControl('')
+    })
+    this.Coustomer=new FormGroup(Group)
 
     this.Coustomer= this.fb.group({
       FirstName:['amin',Validators.required],
       LastName:['' ,[Validators.required,Validators.minLength(2),Validators.maxLength(20)]],
       CoustomerCode:['',[Validators.required,Validators.minLength(2),Validators.maxLength(20)]],
       Email:['',[Validators.required,Validators.email]],
+      
+      AccountInfoForm:this.fb.group({
+        AcountNumber:['',Validators.required],
+        AcountTitle:['',Validators.required],
+        Sheba:['',Validators.required],
+      }),
+
+      AddressList:this.fb.array([this.initAddressForm()])
 
      })
 
@@ -45,6 +61,35 @@ savea(){
 
   console.log(this.Coustomer.value);
   
+}
+get AccountInfoForm(){
+  return this.Coustomer.get('AccountInfoForm')
+}
+
+get addressArray(){
+  return this.Coustomer .get('AddressList')as FormArray
+
+}
+
+initAddressForm(){
+return this.fb.group({
+  type:[''],
+  address:[''],
+})
+}
+
+addNewAddress(event){
+  event.preventDefault() 
+  this.addressArray.push(this.initAddressForm());
+}
+RemoveAddress(event,index){
+  event.preventDefault();
+  this.addressArray.removeAt(index)
+}
+
+onsubmit(){
+console.log(this.Formtemplate.values);
+
 }
 
 }
